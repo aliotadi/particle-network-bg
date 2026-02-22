9f8a08bad08969e8b149ee0296b7e1845f2337ef
ali otadi <ali.78.otadi@gmail.com>
2026-02-22 20:22:49 +0330

    updated base in demos

--- demo/react/vite.config.ts
+++ demo/react/vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE || "/",
+  base: "/particle-network-bg/react/",
 });
+
--- demo/vanilla/vite.config.js
+++ demo/vanilla/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.BASE || "/",
+  base: "/particle-network-bg/vanilla/",
 });
+
